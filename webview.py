dff4864ddc9a13355c35f0ebbc6b41a364f46ae2
Nicolas Pace <nicolas.pace@unixono.com.ar>
2013-07-10 16:16:42 -0300

    added code

---
+++ webview.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+import sys
+
+from functools import partial
+from PySide import QtCore, QtGui, QtWebKit
+
+
+def _loadFinished(webview, url, ok):
+    print "Load finished", ok
+    if not ok:
+        print "Reloading..."
+        webview.load(url)
+
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    web = QtWebKit.QWebView()
+    url = QtCore.QUrl(sys.argv[1])
+
+    web.loadFinished.connect(partial(_loadFinished, web, url))
+
+    web.load(url)
+    web.showFullScreen()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
