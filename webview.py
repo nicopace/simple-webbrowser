0225aaa0bbf57035dcae110705a888a6d0754450
Nicolás Pace <nicopace@gmail.com>
2014-03-31 17:18:04 -0300

    Ahora se escucha al QNetworkManager para tener mas detalle de porque fallo el request. Ademas se usa una URL alternativa local, que se cargará cuando no cargue la principal.

--- webview.py
+++ webview.py
@@ -3,23 +3,47 @@ import sys
 
 from functools import partial
 from PyQt4 import QtCore, QtGui, QtWebKit
+from PyQt4.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
 
 
-def _loadFinished(webview, url, ok):
+def _loadFinished(webview, url, alternativeurl, reply):
+    requesturl = reply.request().url()
+    if requesturl != url:
+        if requesturl == alternativeurl:
+            # Se supone que la url alternativa es local
+            print "url alternativa cargada, reintentando con la primaria en 5 segs"
+            QtCore.QTimer.singleShot(5000, partial(_pageTimeout, webview, url))
+
+        return
+
+    error = reply.error()
+    ok = error == QNetworkReply.NoError
+
     print "Load finished", ok
     if not ok:
+        print "network error %i" % (error, )
         print "Reloading..."
-        webview.load(url)
+        webview.load(alternativeurl)
+    else:
+        timeoutSeconds = 60
+        timer = QtCore.QTimer()
+        timer.timeout.connect(partial(_pageTimeout, webview, url))
+        timer.start(timeoutSeconds * 1000)
 
+
+def _pageTimeout(webview, url):
+    webview.load(url)
 
 def main():
     app = QtGui.QApplication(sys.argv)
     web = QtWebKit.QWebView()
     url = QtCore.QUrl(sys.argv[1])
+    alternativeurl = QtCore.QUrl(sys.argv[2])
 
-    web.loadFinished.connect(partial(_loadFinished, web, url))
+    nam = web.page().networkAccessManager()
+    nam.finished.connect(partial(_loadFinished, web, url, alternativeurl))
 
-    web.load(url)
+    web.load(alternativeurl)
     web.showFullScreen()
     sys.exit(app.exec_())
 
